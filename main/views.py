670faac313426f389fc0d9c256b50a264183384a
Андрей Лисин <foxers100@gmail.com>
2021-01-20 04:07:58 +0300

    added paination on index

--- main/views.py
+++ main/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from main.models import Article
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
+
+def paginate(objects_list, request, per_page=10):
+    paginator = Paginator(objects_list, per_page)
+    page_number = request.GET.get('page')
+    return paginator.get_page(page_number)
 
 def index(request):
     questions = Article.objects.all()
-    return render(request, 'index.html', {'questions': questions})
+    page = paginate(questions, request, 2)
+    return render(request, 'index.html', {
+        'page': page,
+        'page_end_diff': page.paginator.num_pages - page.number,
+        'user': request.user,
+    })
 
 def settings(request):
     return render(request, 'settings.html')
