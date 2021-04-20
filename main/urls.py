16dfebfc2f77df411676cb76bf76886a9f6a2040
Андрей Лисин <foxers100@gmail.com>
2021-04-20 20:08:04 +0300

    fixed routing, layout and isolated pagination

--- main/urls.py
+++ main/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('settings', views.settings),
-    path('login', views.login),
+    path('', views.index, name='index'),
+    path('settings', views.settings, name='settings'),
+    path('login', views.login, name='login'),
     path('question/<int:question_id>/', views.question, name='question'),
-    path('ask', views.ask),
-    path('tag', views.tag),
-    path('registration', views.registration)
+    path('ask', views.ask, name='ask'),
+    path('tag', views.tag, name='tag'),
+    path('registration', views.registration, name='registration')
 ]
